88d76b7559638384d16cecadb490c4ec2348619c
cotsao <codytsao@gmail.com>
2021-07-11 12:42:32 -0700

    Commit 1 - data types

---
+++ app.js
@@ -0,0 +1,21 @@
+/* Cody Tsao
+SEI 7.06
+Mixed Data Types */
+
+// 1. Data Types
+
+/* 
+A light switch that can be either on or off.
+Boolean, lightswitch = true or false
+A user's email address.
+String, string just contains email address
+A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+Object, variables for each spaceship component
+A list of student names from our class.
+Array, each index contains a name
+A list of student names from our class, each with a location.
+Array of Objects, each index contains the object that contains a string w/ the location
+A list of student names from our class, each with a location and each with a list of favorite tv shows.
+Array of Objects, each index contains an object with each of the required information
+ */
+
