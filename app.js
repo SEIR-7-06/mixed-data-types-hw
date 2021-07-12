b61de154d3889d90b7530204175ec3ea99113bbe
erininabox <erininabox@gmail.com>
2021-07-12 16:51:28 -0700

    Commit 1 - data structures

---
+++ app.js
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////
+// MIXED DATA TYPES HOMEWORK - ERIN OGDEN
+
+// QUESTION 1 DATA TYPES
+
+// A light switch that can be either on or off.
+// BOOLEAN
+let lightSwitch = true;
+
+// A user's email address
+// STRING
+let userEmail = 'erininabox@gmail.com';
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// ARRAY
+const spaceship = ['hull', 'laswer blasters', 'tractor beam', 'warp drive'];
+
+// A list of student names from our class.
+// ARRAY
+const studentNames = ['Erin', 'Brooke', 'Megan', 'Nicole', 'Stephany', 'Gonzalo', 'George', 'Blake', 'Cody', 'DK', 'Greg', 'Jack', 'Marshall', 'Marshawn', 'Neeraj', 'Satya', ];
+
+// A list of student names from our class, each with a location.
+// OBJECT
+const student1 = {
+    studentFirstName: 'Erin',
+    studentLocation: 'San Bruno, CA',
+};
+// repeat this for additional students
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// OBJECT
+const student2 = {
+    studentFirstName: 'Erin',
+    studentLocation: 'San Bruno, CA',
+    faveTVShows: ['Nancy Drew', 'Handmaids Tale', 'Star Trek'],
+}
+
+
+// QUESTION 2 - TAKE IT EASY
