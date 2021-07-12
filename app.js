e1a246bf9524f0dd9615b1d65f51d66b6f3901e9
off2mxrs <off2mxrs@gmail.com>
2021-07-12 16:38:49 -0700

    data types

---
+++ app.js
@@ -0,0 +1,46 @@
+// 1. Data types
+
+// Boolean
+const lightOn = true;
+
+// String
+const emailAddress = "off2mxrs@gmail.com";
+
+// Object
+const spaceship = {
+    build: 'hull', 
+    weapon: 'laser blasters',
+    weapon2: 'tractor beam',
+    speed: 'warp drive',
+
+}
+
+// Array
+const students = [
+    'Marshawn',
+    'Neeraj',
+    'Stephany',
+    'Satya',
+]
+
+// Objects nested
+const studentLocation = {
+    marshawn: {
+        location: 'Sacramento'
+    },
+    greg: {
+        location: 'San Fran'
+    }, 
+}
+
+// Objects and Arrays
+const studentInfo = {
+    marshawn: {
+        location: 'Sacramento',
+        tvShow: ['GOT', 'Vikings', 'Snowfall'],
+    },
+    greg: {
+        location: 'San Fran',
+        tvShow: ['SNL', 'Saved by the bell', 'the Office'],
+    }, 
+}
