39546b0c9089f80c6788b5864d934e900a49025f
gonzzoh <gmeza0118@gmail.com>
2021-07-12 21:03:00 -0400

    crazy object

--- apps.js
+++ apps.js
@@ -1,4 +1,4 @@
-// DATA TYPES QUESTIONS
+// DATA TYPES QUESTIONS //
 
 // Booleans - (let islightOn = true)
 // String - (let email = "fakemail@fmail.com")
@@ -10,8 +10,8 @@
 // object - ( const classId = {
 //     Gonz: {"NJ", Invincible, Rick and Morty"}; }
 
-//////////////////////////////////////////////////
-// TAKE IT EASY 
+//////////////////////////////////////////////////////////////////////
+// TAKE IT EASY //
 
 const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
@@ -27,3 +27,46 @@ stalker: function(){
 }
 }
 Gonzalo.stalker();
+
+///////////////////////////////////////////////////////////////////////
+// CRAZY OBJECTS //
+
+const crazyObject = {
+    taco: [
+      {
+        meat: 'steak',
+        cheese: ['panela', 'queso', 'chihuahua']
+      },
+      {
+        meat: 'chicken',
+        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+      },
+    ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [
+        {
+          name: "Jeff",
+          occupation: "manager"
+        },
+        {
+          name: "funkhauser",
+          occupation: "tv dude"
+        },
+        {
+          name: "susie",
+          occupation: "jeffs wife",
+          favourtieHobby: "Swearing at Larry and Jeff"
+        },
+      ]
+    }
+  }
+
+  console.log(crazyObject.taco[1].salsa[5]);
+  console.log(crazyObject.larry.quotes[0]);
+  console.log(crazyObject.larry.characters[2].favourtieHobby);
+  console.log(crazyObject.larry.nicknames[1]);
+  console.log(crazyObject.larry.characters[1]);
+
+
