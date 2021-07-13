781e2fd06fdf57d893ab9af2274ff37ad439d3ce
gregdanko92 <gregdanko92@gmail.com>
2021-07-13 09:16:21 -0700

    data types

---
+++ app.js
@@ -0,0 +1,30 @@
+//1. 
+let lightSwitch = true
+
+const userEmailAddress = "email@address.com"
+
+const spaceShip = ['hull', 'lasers blasters', 'tractor beam', 'warp drive']
+
+const students = ['Tim', 'Jim', 'Pam']
+
+const studentsWLocations = {
+    Larry: {
+        firstName: "Larry",
+        lastName: "Smith",
+        location: "San Antonio",
+        tvShows: ["The Sopranos", "Boardwalk Empire", "Succession"]
+    },
+    Barry: {
+        firstName: "Barack",
+        lastName:"Obama",
+        location: "Washington, D.C.",
+        tvShows: ["South Park", "Mr. Show", "Arrested Development"]
+    },
+    Gary: {
+        firstName: "Gary",
+        lastName: "The Snail",
+        location: "Bikini Bottom",
+        tvShows: ["NBA Live", "SportsCenter", "I think you should leave" ]
+    }
+}
+
