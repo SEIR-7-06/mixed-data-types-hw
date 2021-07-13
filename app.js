8e5bc160fcd43718f1e16738dca76a4dfa319b41
marshallwatrous <marshallwatrous95@gmail.com>
2021-07-13 16:49:59 -0700

    data types

---
+++ app.js
@@ -0,0 +1,30 @@
+// 1. Data types
+
+// * A boolean where true means is on and false means off.  I would use a function tha would return true if it was on and false if it was off.
+// * I would use a string for the email.  The data structure I would use would be an object where one of the key-value pairs is email address and their email.
+// * I would make an array of strings for the spaceship with the hull, laser blasters, tractor beams and warp drive as indices.
+// * The names would be strings, and they would be in array.
+// * The names and locations would be strings and they would be in an object of the student.
+// * The names, locations, and shows would be strings in an object of the student.  The shows would be in an array in the obect.
+
+// 2. Take it easy
+
+// 1.
+const rainbow = ["red", "orange", "yellow,", "green", "blue", "indigo", "violet"];
+
+// 2. 
+
+console.log(rainbow[4]);
+
+// 3.
+
+const marshall = {
+    favoriteFood: "pizza",
+    hobby: "walking",
+    hometown: "Seattle",
+    favoriteDatatype: "boolean"
+};
+
+// 4.
+
+console.log(marshall.hobby);
