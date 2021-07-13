7f5783bdfe1b5d509af340c46738839c665aa816
nndeto <n_ndeto@yahoo.com>
2021-07-12 20:33:56 -0400

    section one completed

---
+++ app.js
@@ -0,0 +1,63 @@
+////Data Types
+
+//a light switch that's on or off --> i would use a boolean value 
+// and a conditional that can check if lightOn is true or false and then act on it
+//so that if it isn't on/off it can be changed
+//code snippet: 
+// let lightOn = false;
+// if (lightOn) {
+//     console.log("Your lights are already on.");
+// } else if (!lightOn) {
+//     console.log("Let's turn the lights on. One moment");
+//     lightOn = true;
+//     console.log(lightOn);
+//     console.log("See, we've turned the lights on.")
+// };
+
+//user's email address --> i would save that as a string
+//that way when there is no email address it can stay empty
+//and also take numbers and letters with ease
+//code snippet:
+//let email = "123@yahoo.com";
+
+//spaceship --> object because it has so many properties
+//code snippet:
+// const spaceship = {
+//     warpDrive: true,
+//     beamType: "tractor blasters",
+
+// }
+
+//student names list --> array
+//let studentList = ["John", "Luke", "David","Marcus"];
+
+//student name w/location --> object within array
+//code snippet:
+// const students = [
+//     {name: "Edith",
+//      location: "Georgia"},
+//     {name:"Lorelei",
+//     location: "Estonia"}
+// ]
+
+//student names w/location/tv shows --> an array w/nested objects
+//code snippet:
+// const students = [
+//     {name: "Danity",
+//     location: "South Central",
+//     favoriteTv: ["Law and Order:SVU", "Hoarders",]
+//     },
+//     {name: "Monica",
+//     location: "NYC",
+//     favoriteTv: ["Rick and Morty", "Bob's Burgers", "Archer"]
+//     },
+//     {name: "Corey",
+//     location: "Atlanta",
+//     favoriteTv: ["The Wonderful World of Gumball", "Regular Show", "Adventure Time"]
+//     },
+//     {name: "Danity",
+//     location: "South Central",
+//     favoriteTv: ["MasterChef", "House Hunters", "Love it or List It"]
+//     }
+// ];
+
