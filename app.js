32d66d155fc0692ac8ab8f46ebc6b4aa57de3fb1
meganroberta80 <meganroberta89@gmail.com>
2021-07-12 20:54:02 -0700

    Data types

---
+++ app.js
@@ -0,0 +1,66 @@
+// A light switch that can be either on or off.
+// Boolean
+lightSwitchOn: true 
+
+// A user's email address.
+// String
+email: 'johndoe123@gmail.com' 
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// Array
+const spaceshipParts = [
+    hull,
+    laserBlasters,
+    tractorBeam, 
+    warpDrive
+]
+
+// A list of student names from our class.
+// Array
+const sei706 = [
+    Brooke,
+    Cody,
+    DK, 
+    Erin, 
+    George, 
+    Gonzalo, 
+    Greg, 
+    Jack, 
+    Marshall,
+    Marshawn, 
+    Megan, 
+    Neeraj, 
+    Nicole, 
+    Satya, 
+    Stephany 
+] 
+// A list of student names from our class, each with a location.
+// Array, Objects 
+const sei706Location = [
+    {firstName: 'Brooke',
+     city: 'Mesa'
+    },
+    {firstName: 'DK',
+    city: 'Rockland'
+    },
+    {firstName: 'Erin',
+    city: 'San Bruno'
+    }
+] 
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// Array, Objects
+const sei706Shows = [
+    {firstName: 'Stephany',
+    city: 'Escondido',
+    favoriteTVShows: ['show1', 'show2', 'show3']
+    },
+    {firstName: 'Satya',
+    city: 'San Francisco',
+    favoriteTVShows: ['show1', 'show2', 'show3']
+    },
+    {firstName: 'Nicole',
+    city: 'South Bend',
+    favoriteTVShows: ['show1', 'show2', 'show3']
+    }
+]
