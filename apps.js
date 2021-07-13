70ddc06b44f3067cba7561194a78f4a2ac3cdc21
gonzzoh <gmeza0118@gmail.com>
2021-07-12 20:44:44 -0400

    data structures

---
+++ apps.js
@@ -0,0 +1,29 @@
+// DATA TYPES QUESTIONS
+
+// Booleans - (let islightOn = true)
+// String - (let email = "fakemail@fmail.com")
+// Array - (const spaceship=[hull, laser blasters...])
+// Array - (const students=[Arifa, Siara, John, Miguel])
+// Object - ( const classId = {
+//     Gonz: "NJ"
+// } )
+// object - ( const classId = {
+//     Gonz: {"NJ", Invincible, Rick and Morty"}; }
+
+//////////////////////////////////////////////////
+// TAKE IT EASY 
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+console.log(rainbow[4])
+
+const Gonzalo = {
+    favoriteFood: 'Aji de Gallina',
+    hobby: 'working on car',
+    residence: 'Roselle Park',
+    favDataType: 'Booleans',
+stalker: function(){
+    console.log(Gonzalo.hobby)
+}
+}
+Gonzalo.stalker();
