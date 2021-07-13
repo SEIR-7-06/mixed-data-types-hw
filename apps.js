ddd7d01fd92be009cb7414e60b33e3d4dd7f21da
gonzzoh <gmeza0118@gmail.com>
2021-07-12 21:13:10 -0400

    object-ception

--- apps.js
+++ apps.js
@@ -13,60 +13,83 @@
 //////////////////////////////////////////////////////////////////////
 // TAKE IT EASY //
 
-const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-console.log(rainbow[4])
+// console.log(rainbow[4])
 
-const Gonzalo = {
-    favoriteFood: 'Aji de Gallina',
-    hobby: 'working on car',
-    residence: 'Roselle Park',
-    favDataType: 'Booleans',
-stalker: function(){
-    console.log(Gonzalo.hobby)
-}
-}
-Gonzalo.stalker();
+// const Gonzalo = {
+//     favoriteFood: 'Aji de Gallina',
+//     hobby: 'working on car',
+//     residence: 'Roselle Park',
+//     favDataType: 'Booleans',
+// stalker: function(){
+//     console.log(Gonzalo.hobby)
+// }
+// }
+// Gonzalo.stalker();
 
 ///////////////////////////////////////////////////////////////////////
 // CRAZY OBJECTS //
 
-const crazyObject = {
-    taco: [
-      {
-        meat: 'steak',
-        cheese: ['panela', 'queso', 'chihuahua']
-      },
-      {
-        meat: 'chicken',
-        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
-      },
-    ],
-    larry: {
-      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-      characters: [
-        {
-          name: "Jeff",
-          occupation: "manager"
-        },
-        {
-          name: "funkhauser",
-          occupation: "tv dude"
-        },
-        {
-          name: "susie",
-          occupation: "jeffs wife",
-          favourtieHobby: "Swearing at Larry and Jeff"
-        },
-      ]
+// const crazyObject = {
+//     taco: [
+//       {
+//         meat: 'steak',
+//         cheese: ['panela', 'queso', 'chihuahua']
+//       },
+//       {
+//         meat: 'chicken',
+//         salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+//       },
+//     ],
+//     larry: {
+//       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//       characters: [
+//         {
+//           name: "Jeff",
+//           occupation: "manager"
+//         },
+//         {
+//           name: "funkhauser",
+//           occupation: "tv dude"
+//         },
+//         {
+//           name: "susie",
+//           occupation: "jeffs wife",
+//           favourtieHobby: "Swearing at Larry and Jeff"
+//         },
+//       ]
+//     }
+//   }
+
+//   console.log(crazyObject.taco[1].salsa[5]);
+//   console.log(crazyObject.larry.quotes[0]);
+//   console.log(crazyObject.larry.characters[2].favourtieHobby);
+//   console.log(crazyObject.larry.nicknames[1]);
+//   console.log(crazyObject.larry.characters[1]);
+
+/////////////////////////////////////////////////////////////////////
+// OBJECT CEPTION //
+
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
     }
-  }
+ }
 
-  console.log(crazyObject.taco[1].salsa[5]);
-  console.log(crazyObject.larry.quotes[0]);
-  console.log(crazyObject.larry.characters[2].favourtieHobby);
-  console.log(crazyObject.larry.nicknames[1]);
-  console.log(crazyObject.larry.characters[1]);
-
-
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+ console.log(inception);
+ 
