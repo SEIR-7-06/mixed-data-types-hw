aea9f8dff7949b0b1eb422ff12c041c32daf6893
DataPlant <dataplant@outlook.com>
2021-07-14 19:15:40 -0400

    crazyObject

--- app.js
+++ app.js
@@ -37,3 +37,39 @@ let dkNam = {
     favData: 'Arrays',
 }
 console.log(dkNam.hobby)
+///////////////////Crazy Objects
+const crazyObject = {
+    taco: [
+      {
+        meat: 'steak',
+        cheese: ['panela', 'queso', 'chihuahua']
+      },
+      {
+        meat: 'chicken',
+        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+      },
+    ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [
+        {
+          name: "Jeff",
+          occupation: "manager"
+        },
+        {
+          name: "funkhauser",
+          occupation: "tv dude"
+        },
+        {
+          name: "susie",
+          occupation: "jeffs wife",
+          favourtieHobby: "Swearing at Larry and Jeff"
+        },
+      ]
+    }
+  }
+  ////////////Crazy Objects (contd.)
+  console.log(crazyObject['taco'][1].salsa[5])
+  console.log(crazyObject.larry['quotes'][0])
+  console.log(crazyObject.larry.characters[2]['favourtieHobby'])
