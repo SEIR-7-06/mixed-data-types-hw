725fcc10342062e3ff5af1efeb913a5642683d44
DataPlant <dataplant@outlook.com>
2021-07-13 20:13:16 -0400

    data types

---
+++ app.js
@@ -0,0 +1,23 @@
+//   * A light switch that can be either on or off.
+//  boolean
+//  light = true;
+//   * A user's email address.
+//  string
+//  let emailAddress = Dataplant@outlook.com
+//   * A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//  object
+//  spaceship ={
+//     hull: 100
+//     tractor-beam: 100
+//     warp-drive: 100
+//  }
+//   * A list of student names from our class.
+//  Array
+//  let listNames = []
+//   * A list of student names from our class, each with a location.
+//  Array in Array
+//  let studentNameLoc = [[Max, New York],[Jane, Los Angeles]]
+//   * A list of student names from our class, each with a location and each with a list of favorite tv shows.
+//  objects in array
+//  let arrayObj = [student1={}, student2={}]
+
