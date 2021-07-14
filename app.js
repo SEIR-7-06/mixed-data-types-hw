561b2f0533e4911af79d336cbadce6fdca5294ce
xsatyax <satyasette1@gmail.com>
2021-07-14 16:06:49 -0700

    Commit: "data types"

---
+++ app.js
@@ -0,0 +1,44 @@
+// A light switch that can be either on or off.
+// BOOLEAN
+let light = true
+
+// A user's email address.
+// STRING
+    let email = 'satyasette1@gmail.com'
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// ARRAY
+    const spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']
+
+// A list of student names from our class.
+// ARRAY
+    const students = ['Stephany', 'Megan', 'Jack']
+
+// A list of student names from our class, each with a location.
+// OBJECT
+    const students2 = [
+        {name : 'Stephany', "location" : 'location' },
+        {name : 'Megan', "location" : 'location' },
+        {name : 'Jack', "location" : 'Santa Cruz' }
+        ]
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// OBJECT
+const students3 = [
+    {name : 'Stephany', 
+    location : 'location1',
+    favoriteShows : ['show 1', 'show 2', 'show 3']
+},
+
+    {name : 'Megan',
+     location : 'location2', 
+     favoriteShows : ['show 1', 'show 2', 'show 3']
+    },
+
+    {'name' : 'Jack',
+     "location" : 'location3',
+     favoriteShows : ['show 1', 'show 2', 'show 3']
+    }
+    ]
+
+// 🔴 Commit: "data types"
